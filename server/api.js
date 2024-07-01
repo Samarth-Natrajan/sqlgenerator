5d48455d8c1e4d12c93f87aa27f12a1c82039042
Samarth Natrajan <samarthshridharnatrajan.bt21ece@pec.edu.in>
2024-07-01 15:36:41 +0530

    error for api key resolved

--- server/api.js
+++ server/api.js
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 import dotenv from "dotenv";
 dotenv.config();
 
-const openaiApiKey = process.env.OPENAI_API_KEY;
+const openaiApiKey = process.env.OPENAI_API_KEY || 12;
 
 if (!openaiApiKey) {
   console.error('OPENAI_API_KEY is not set.');
