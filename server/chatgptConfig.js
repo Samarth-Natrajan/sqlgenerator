ca20b5df71486073ee4e8bb46b1131872a9f1ebd
Samarth Natrajan <samarthshridharnatrajan.bt21ece@pec.edu.in>
2024-07-03 17:15:59 +0530

    chatgpt config seperated from api function

---
+++ server/chatgptConfig.js
@@ -0,0 +1,27 @@
+import { Configuration, OpenAIApi } from "openai";
+import {ChatPromptTemplate,SystemMessagePromptTemplate,HumanMessagePromptTemplate} from "@langchain/core/prompts"
+import { ChatOpenAI } from "@langchain/openai";
+import dotenv from "dotenv";
+dotenv.config();
+
+// const openaiApiKey = process.env.OPENAI_API_KEY;
+
+// if (!openaiApiKey) {
+//   console.error('OPENAI_API_KEY is not set.');
+//   process.exit(1);
+// }
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+const chat = new ChatOpenAI({openAIApiKey:process.env.OPENAI_API_KEY})
+  const systemMessagePrompt = SystemMessagePromptTemplate.fromTemplate(
+    "You are an sql query generator. You will get a human generated statement in text form which will be in english describing a problem related to sql queries.Your task is to convert the problem in english language or situation into an sql query.You might also be required to correct a query given by user if it is not correct according to the desired result.If the input to you is out of this domain you should say 'Sorry I do not speciallize in this.I am only aware about sql query generation .'"
+  )
+  const humanMessagePrompt = HumanMessagePromptTemplate.fromTemplate("{asked_question}");
+  const chatprompt = ChatPromptTemplate.fromMessages([
+    systemMessagePrompt,humanMessagePrompt
+  ])
+
+export {chat,chatprompt};
--- server/generate.js
+++ server/generate.js
@@ -1,6 +1,7 @@
 
-import { ChatOpenAI } from "@langchain/openai";
+
 import dotenv from "dotenv";
+import {chat,chatprompt} from "./chatgptConfig";
 dotenv.config();
 import {ChatPromptTemplate,SystemMessagePromptTemplate,HumanMessagePromptTemplate} from "@langchain/core/prompts"
 const generate = async (queryDescription) => {
@@ -32,14 +33,7 @@ const generate = async (queryDescription) => {
 
   // const sqlQuery = await chatGPT(queryDescription);
   // return sqlQuery;
-  const chat = new ChatOpenAI({openAIApiKey:process.env.OPENAI_API_KEY})
-  const systemMessagePrompt = SystemMessagePromptTemplate.fromTemplate(
-    "You are an sql query generator. You will get a human generated statement in text form which will be in english describing a problem related to sql queries.Your task is to convert the problem in english language or situation into an sql query.You might also be required to correct a query given by user if it is not correct according to the desired result.If the input to you is out of this domain you should say 'Sorry I do not speciallize in this.I am only aware about sql query generation .'"
-  )
-  const humanMessagePrompt = HumanMessagePromptTemplate.fromTemplate("{asked_question}");
-  const chatprompt = ChatPromptTemplate.fromMessages([
-    systemMessagePrompt,humanMessagePrompt
-  ])
+  
   const formattedChatPrompt = await chatprompt.formatMessages({
     asked_question:queryDescription
   })
