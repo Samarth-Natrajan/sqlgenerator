86d6bbe5d9baf5c9656e530fcfc165331575f614
Samarth Natrajan <samarthshridharnatrajan.bt21ece@pec.edu.in>
2024-07-07 13:01:59 +0530

    msg line added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -31,6 +31,7 @@ export default function App() {
     <main className={styles.main}>
       <img src={sqlServer} className={styles.icon} alt="SQL server" />
       <h3>Generate SQL</h3>
+      <h4>Generate, Fix & Improve your SQL Queries</h4>
       <form onSubmit={onSubmit}>
         <textarea
           type="text"
